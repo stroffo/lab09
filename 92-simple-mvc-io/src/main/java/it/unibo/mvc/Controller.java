b336e0ff1ba20c36bcbacc351e9a7c863a5a73fb
Nicola Stroffolino <nicola.stroffolino@studio.unibo.it>
2025-11-20 01:21:49 +0100

    Ex.92 "Vibe" coded the Controller class without proper testing

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currentFile = new File(
+        System.getProperty("user.home")
+        + System.getProperty("file.separator")
+        + "output.txt"
+    );
 
+    public void setCurrentFile(final File file) {
+        this.currentFile = file;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public String getFilePath() {
+        return currentFile.getAbsolutePath();
+    }
+
+    public void writeToFile(final String message) throws IOException {
+        try (
+            final BufferedWriter w = new BufferedWriter(new FileWriter(currentFile))
+        ) {
+            w.write(message);
+        } catch (final IOException e) {
+            throw e;
+        }
+    }
 }
