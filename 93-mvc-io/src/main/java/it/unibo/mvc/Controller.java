7135828743a1de54885a31eb80aa0009f3403e3a
Nicola Stroffolino <nicola.stroffolino@studio.unibo.it>
2025-11-23 00:53:59 +0100

    Ex.93 SimpleController "skeleton" implementation

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -4,5 +4,11 @@ package it.unibo.mvc;
  *
  */
 public interface Controller {
+    public void enqueue(final String str) throws NullPointerException;
 
+    public String dequeueAndPrint() throws IllegalStateException;
+
+    public String peek();
+
+    public java.util.List<String> getPrintHisory();
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,30 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
 
+    @Override
+    public void enqueue(String str) {
+
+    }
+
+    @Override
+    public String dequeueAndPrint() {
+        return "";
+    }
+
+    @Override
+    public String peek() {
+        return "";
+    }
+
+    @Override
+    public List<String> getPrintHisory() {
+        return List.of();
+    }
 }
